ccdf892c9f0621b7967e1da994ec9c05f67de415
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-03-19 15:51:55 -0700

    Work

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -4,13 +4,13 @@ import { ContractType, type Experience } from '../types';
 
 export const items: Array<Experience> = [
 	{
-		slug: 'open-sourcer',
+		slug: '',
 		company: 'Meta (via Solvd Inc.)',
 		description: 'As a Test Automation Engineer at Meta, my role focused on ensuring compliance with FTC orders by conducting in-depth peer reviews across Java, PHP, React, Kotlin, Swift, and Objective-C. I reviewed an average of 8-15 daily code submissions, auditing documentation, data pipelines, and databases to maintain a 100% FTC-compliant code base. Additionally, I contributed to the validation of in-house automation frameworks by manually testing automated review results, ensuring accuracy in compliance enforcement and streamlining the audit process.',
-		contract: ContractType.SelfEmployed,
+		contract: ContractType.FullTime,
 		type: 'Software Development',
 		location: 'Home',
-		period: { from: new Date() },
+		period: { from: new Date(2024, 7, 1), to: new Date(2025, 2, 1) },
 		skills: getSkills('ts', 'js'),
 		name: 'Test Automation Engineer',
 		color: 'purple',
