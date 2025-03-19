d71627764d18e297c51c1cd1206603221bc65e1c
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-03-19 16:06:43 -0700

    Working

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -34,19 +34,49 @@ export const items: Array<Experience> = [
 		shortDescription: 'Learned the ropes of test automation - focused primarily on learning Selenium and Java Concepts.'
 	},
 	{
-		slug: 'software-freelance-junior',
-		company: 'Self-employed',
-		description: 'Creating awesome applications for customers.',
-		contract: ContractType.Freelance,
-		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date(2022, 0, 1), to: new Date() },
+		slug: 'key-accounts-logistics',
+		company: 'Biocair',
+		description: 'As a Key Accounts Logistics Specialist at Biocair, my role expanded to managing a broader range of clients while also diving into data analysis and performance tracking. I was responsible for tracking KPIs across all key accounts, analyzing client lanes, pricing, and vendor performance to identify trends and improve service consistency. By comparing logistics strategies across accounts, I helped optimize operations, strengthen client relationships, and enhance overall efficiency.',
+		contract: ContractType.FullTime,
+		type: 'Pharmaceutical Supply Chain',
+		location: 'Carson, CA',
+		period: { from: new Date(2024, 05, 1), to: new Date(2024, 07, 1) },
 		skills: getSkills('css', 'html', 'js'),
-		name: 'Junior Freelancer',
-		color: 'green',
+		name: 'Key Accounts Logistics Specialist',
+		color: 'blue',
 		links: [],
 		logo: Assets.Unknown,
-		shortDescription: 'Creating awesome applications for customers.'
+		shortDescription: 'Played an analytical role in tracking KPIs across multiple Biotech customers.'
+	},
+	{
+		slug: 'cell-and-gene-therapy-logistics',
+		company: 'Biocair',
+		description: 'As a Cell and Gene Therapy Logistics Specialist at Biocair, I managed global supply chain operations, coordinating 1,000+ shipments of biologics, medical devices, and clinical trial therapies. I ensured time-sensitive deliveries while adhering to strict regulatory requirements from agencies such as the FDA, CDC, USDA, and other global regulatory bodies.',
+		contract: ContractType.FullTime,
+		type: 'Pharmaceutical Supply Chain',
+		location: 'Carson, CA',
+		period: { from: new Date(2023, 04, 1), to: new Date(2024, 05, 1) },
+		skills: getSkills('css', 'html', 'js'),
+		name: 'Cell and Gene Therapy Logistics Specialist',
+		color: 'blue',
+		links: [],
+		logo: Assets.Unknown,
+		shortDescription: 'Handled all global supply chain operations for multiple Biotech customers.'
+	},
+	{
+		slug: 'air-griley',
+		company: 'Griley Air Freight',
+		description: 'As an Air Freight Specialist at Griley Air Freight, I served as an on-site freight coordinator, managing logistics at multiple warehouse locations. I assessed freight needs, scheduled and rotated drivers, coordinated warehouse loading for airline deliveries, and ensured all documentation was accurate and error-free to keep shipments moving efficiently.',
+		contract: ContractType.FullTime,
+		type: 'Air Freight Transportation',
+		location: 'Hawthorne, CA',
+		period: { from: new Date(2022, 02, 1), to: new Date(2023, 03, 1) },
+		skills: getSkills('css', 'html', 'js'),
+		name: 'Logistics Specialist',
+		color: 'red',
+		links: [],
+		logo: Assets.Unknown,
+		shortDescription: 'Acted as a liaison between multiple teams and oversaw outbound warehouse operations.'
 	}
 ];
 
