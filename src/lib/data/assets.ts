0bdc233a973c6f699d36c04e1bed444e5c9c4fc2
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-03-19 17:04:46 -0700

    Updating Assets

--- src/lib/data/assets.ts
+++ src/lib/data/assets.ts
@@ -73,14 +73,14 @@ const Assets = {
 	Postcss: a('postcss.svg'),
 	WGU: a('wgu.svg'),
 	SpringBoot: a('Spring_Boot.svg'),
-	solvd: a('solvd.png'),
-	snhu: a('SNHU.svg'),
-	portx: a('portx.png'),
-	meta: a('meta.svg'),
-	griley: a('griley.png'),
-	cerritos: a('cerritos.png'),
-	biocair: a('Biocair Black.svg'),
-	apex: a('apex.png')
+	Solvd: a('solvd.png'),
+	Snhu: a('SNHU.svg'),
+	Portx: a('portx.png'),
+	Meta: a('meta.svg'),
+	Griley: a('griley.png'),
+	Cerritos: a('cerritos.png'),
+	Biocair: a('Biocair Black.svg'),
+	Apex: a('apex.png')
 };
 
 export default Assets;
