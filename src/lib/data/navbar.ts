781b74fc49f8a3e0195fac931191637254be2786
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-03-20 18:21:32 -0700

    Working on changes

--- src/lib/data/navbar.ts
+++ src/lib/data/navbar.ts
@@ -3,5 +3,6 @@ export const items = [
 	{ title: 'Projects', to: '/projects', icon: 'i-carbon-cube' },
 	{ title: 'Experience', to: '/experience', icon: 'i-carbon-development' },
 	{ title: 'Education', to: '/education', icon: 'i-carbon-education' },
-	{ title: 'Resumé', to: '/resume', icon: 'i-carbon-result' }
+	{ title: 'Resumé', to: '/resume', icon: 'i-carbon-result' },
+	{ title: 'Deeper Look', to: '/deeper-look', icon: 'i-carbon-user-profile-alt' }
 ] as const;
